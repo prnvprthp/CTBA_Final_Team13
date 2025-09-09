f3eb7d16b7febf7920a4f9dbe283be00865c85a8
Pranav Prathap <prnvprthp@gmail.com>
2025-09-09 17:54:58 -0400

    update

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ app.layout = html.Div([
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))  # use Render's PORT or default 8050
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
