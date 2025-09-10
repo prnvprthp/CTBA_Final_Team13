665bd1e0ce7c736c0339cfd9896d594204ecf9ed
Pranav Prathap <63449686+prnvprthp@users.noreply.github.com>
2025-09-10 09:19:35 -0400

    remove debug=true

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ app.layout = html.Div([
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))  #line sourced from GPT
-    app.run(host = '0.0.0.0', port = port, debug=True)
+    app.run(host = '0.0.0.0', port = port, debug=False)
